b9a46b4cd8ff7aad988be59a6c6b95c974d7e52e
Hari <snshari80@gmail.com>
2025-09-13 18:23:36 +0530

    Initial commit

---
+++ script-template/script.py
@@ -0,0 +1,25 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+# ]
+# ///
+
+from datetime import datetime
+
+
+def start_coding():
+    print("Add more Python code to this script to extend functionality!")
+
+
+def date():
+    current_datetime = datetime.now()
+    return current_datetime
+
+
+def main():
+    start_coding()
+    print(date())
+
+
+if __name__ == "__main__":
+    main()
