8aa7fbd20503c6b45b755a8d1aee9dddd82a1737
Hari <snshari80@gmail.com>
2025-09-14 11:57:52 +0530

    fixing

--- app/textFileGenerator.py
+++ app/textFileGenerator.py
@@ -1,15 +1,13 @@
+"""Module to collect contact details from user and save them as a text file on the Desktop."""
 from pathlib import Path
 
 desktop_path = Path.home() / 'OneDrive' / 'Desktop'
 desktop_path.mkdir(parents=True, exist_ok=True)
 
-data = {}
-fields = {}
-
-print('Happy Coding :) Please enter the contact details to convert as a file!')
-
 def get_contact_details():
-
+    data = {}
+    fields = {}
+    
     columns_input = input('Enter the Total Fields number needs to be added: ')
     if columns_input.isdigit() and int(columns_input) > 0:
         columns = int(columns_input)
@@ -22,37 +20,39 @@ def get_contact_details():
         value = input(f'Enter the field name: ')
         fields[field] = value 
     
-    return validateData(data)
+    validate_data(data, fields)
 
-def validateData(data):
+def validate_data(data, fields):
     for key, value in fields.items():
         data[value] = input(f'Enter value for {value}: ')
 
     for key, value in data.items():
-        global file_Name
         if key.lower() == 'name':
             file_Name = value
         if not value:
             print(f"{key} is required. Please try again.")
-            return validateData()
+            return validate_data()
         if key.lower() in ('name') and not value.isalpha():
             print("Name should only contain alphabets. Please try again.")
-            return validateData()
+            return validate_data()
         if key.lower() in ('phone', 'mobile','number') and (not value.isdigit() or len(value) != 10):
             print("Phone number must be a 10-digit number. Please try again.")
-            return validateData()
+            return validate_data()
         if key.lower() == 'email' and ('@' not in value or '.' not in value):
             print("Invalid email format. Please try again.")
-            return validateData()
+            return validate_data()
         else:
             return convert_file(data, file_Name)
 
 def convert_file(data, file_Name):
     filePath = desktop_path / f"{file_Name}.txt"
     print("Creating file at:", filePath)
-    with open(filePath, 'a') as f:
+    with open(filePath, 'a', encoding='utf-8') as f:
          for key, value in data.items():
             f.write(f"{key}: {value}\n")
     print(f"Contact details saved to {filePath}")
 
-get_contact_details()
+if __name__ == "__main__":
+    print('Happy Coding :)')
+    print('This script will help you create a text file with your required details on the Desktop.')
+    get_contact_details()
